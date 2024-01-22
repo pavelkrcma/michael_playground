1b6d8b7864f114cadc574afa2337abbfe7f0b591
Pavel Krcma <krcmap@gmail.com>
2024-01-22 15:49:44 +0100

    space ship game: images, background, preparation for events

--- simple_game_pyglet.py
+++ simple_game_pyglet.py
@@ -1,58 +1,60 @@
 import pyglet
+from pyglet.window import key
 import random
+
+""" Ideas
+Beep: print('\a')
+PNGs: https://www.pngall.com/
+"""
 
 window = pyglet.window.Window()
 
-had_direction_step = 1
+global_score = 0
 
-def tik(t):
-    global had_direction_step
-    screen_x, screen_y = window.get_size()
-    had.x += had_direction_step
-    if had.x >= screen_x - had.width:
-        had_direction_step = -1
-    elif had.x == 0:
-        had_direction_step = 1
+def detect_touching(sprite1, sprite2):
+    return sprite1.x + sprite1.width >= sprite2.x \
+        and sprite1.x <= sprite2.x + sprite2.width \
+        and sprite1.y + sprite1.height >= sprite2.y \
+        and sprite1.y <= sprite2.y + sprite2.height
 
-    had2.x = random.randint(0, screen_x)
-    had2.y = random.randint(0, screen_y)
+def detect_touching_from_scheduler(t):
+    global global_score
+#    if detect_touching(had, had2):
+#        global_score+=1
+#        label.text = f'Score: {global_score}'
 
-def zpracuj_text(text):
-    if text == 'd':
-        had.x += 40
-    if text == 'w':
-        had.y += 40
-    if text == 'e':
-        had.rotation += 20
-    if text == 'a':        
-        had.x -= 40
-    if text == 's':
-        had.y -= 40
-    if text == 'q':
-        had.rotation -= 20
+def zpracuj_stisk_klavesy(symbol, modifier):
+    # symbol parameter is number, not character.
+    # To compare it with character you have to convert char to number using ord() function
+    if symbol == ord('a'):
+        pass
 
 def klik(x, y, tlacitko, mod):
-    if mod == 0:
-        had2.x = x
-        had2.y = y
-    else:
-        had2.rotation -= 25
+    pass
 
 def vykresli():
     window.clear()
-    had.draw()
-    had2.draw()
+    background_img.blit(0, 0)
+    label.draw()
+    ship.draw()
+    asteroid.draw()
 
 window.push_handlers(
-    on_text=zpracuj_text,
+    on_key_press=zpracuj_stisk_klavesy,
     on_draw=vykresli,
     on_mouse_press=klik,
     )
 
-obrazek = pyglet.image.load('had.png')
-obrazek2 = pyglet.image.load('had2.png')
-had = pyglet.sprite.Sprite(obrazek)
-had2 = pyglet.sprite.Sprite(obrazek2)
+spaceship_img = pyglet.image.load('spaceship.png')
+asteroind_img = pyglet.image.load('asteroid.png')
+background_img = pyglet.image.load('background.png')
+ship = pyglet.sprite.Sprite(spaceship_img)
+asteroid = pyglet.sprite.Sprite(asteroind_img)
 
-pyglet.clock.schedule_interval(tik, 1/30)
+label = pyglet.text.Label(f'Score: {global_score}',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=0, y=window.height-36)
+
+pyglet.clock.schedule_interval(detect_touching_from_scheduler, 1/30)
 pyglet.app.run()
